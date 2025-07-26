30df9e1267c2b8cdef38fc698e302a08af8d0706
CaduLopes87 <edu.lopesbezerra@gmail.com>
2025-07-26 18:16:58 -0300

    FEAT: Formata valor dos presentes

--- src/app/features/presentes/presentes.component.ts
+++ src/app/features/presentes/presentes.component.ts
@@ -1,12 +1,13 @@
-import { Component, inject, Inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { IListaPresentes } from '../../shared/models/IListaPresentes.interface';
 import { MainServiceService } from '../../core/main-service.service';
 import { CurrencyPipe } from '@angular/common';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-presentes',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, MatIconModule, MatIcon],
   templateUrl: './presentes.component.html',
   styleUrl: './presentes.component.scss',
 })
